d59e9656078fa535e9b9995131123115dabc793c
Aaron Brown <abrown52@tamu.edu>
2024-09-03 10:50:17 -0500

    Started Example 2 calculations

--- Example2.R
+++ Example2.R
@@ -7,6 +7,8 @@ require(mnormt) # for multivariate normal data generation
 # beta - supplied discriminant vector
 # xtrain, ytrain - training data
 # xtest, ytest - testing data
+# ***xtrain/xtest are matrices with p columns
+# ***ytrain/ytest are vectors; labels: 1 and 2
 #
 # OUTPUT
 # ypred - predicted class membership
@@ -15,23 +17,54 @@ require(mnormt) # for multivariate normal data generation
 classify_for <- function(beta, xtrain, ytrain, xtest, ytest){
   # [ToDo] Code discriminant analysis classifier using for loop
   
-  # Calculate sample means based on training data
-  
+  # Calculate sample means based on training data:
+    # calc sample means by pulling rows for each condition, then calc means
+  x_bar1 <- colMeans(xtrain[ytrain == 1, ]) # can use drop=False
+  x_bar2 <- colMeans(xtrain[ytrain == 2, ]) # can use drop=False
   
   # Calculate class assignments for xtest in a for loop
+  npred <- nrow(xtest)
+  ypred <- rep(1, npred) # pre-specify column of 1's
+  
+  for (i in 1:npred) {
+    # Use h(x) rule to predict: 
+    h1 <- as.numeric(crossprod((xtest[i, ] - xbar1), beta)^2) # (xtest[i, ] - xbar1).T %*% beta squared
+    
+    h2 <-  as.numeric(crossprod((xtest[i, ] - xbar2), beta)^2)
+    
+    # Classify ypred when not default case (h1 < h2):
+    if(h2 < h1){
+      ypred[i] <- 2
+    }
+    
+  }
   
   # Calculate % error using ytest
+  error <- 100 * mean(ypred != ytest) # mean used rather than sum() / npred
+  
   
   # Return predictions and error
   return(list(ypred = ypred, error = error))
 }
 
+
 classify_vec <- function(beta, xtrain, ytrain, xtest, ytest){
   # [ToDo] Try to create vectorized version of classify_for
   
   # Calculate sample means based on training data
+  x_bar1 <- colMeans(xtrain[ytrain == 1, ]) # can use drop=False
+  x_bar2 <- colMeans(xtrain[ytrain == 2, ]) # can use drop=False
+  
+  # Calculate the inner product of the means with beta
+  m1b <- as.numeric(crossprod(xbar1, beta))
+  m2b <- as.numeric(crossprod(xbar2, beta))
+  
+  # Calculate product of xtest with beta
+  xtestb <- xtest %*% beta
   
   # Calculate class assignments for xtest using matrix and vector algebra
+  h1 <- (xtestb - m1b)^2
+  h2 <- (xtestb - m2b)^2
   
   # Calculate % error using ytest
   
